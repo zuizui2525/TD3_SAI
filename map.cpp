e44bc2a09a8e9a2edaef1c4f0d2eedfc14d76396
Ren Akamine <k024g0059@g.neec.ac.jp>
2025-01-21 13:49:40 +0900

    敵による軌跡ブロック除去の処理を実装

--- map.cpp
+++ map.cpp
@@ -60,9 +60,8 @@ void Map::Draw() {
 
 void Map::SetTile(int row, int column, int tileType) {
 	if (row >= 0 && row < mapRow && column >= 0 && column < mapColumn) {
-		map_[row][column] = tileType;  // 軌跡を指定位置に配置
+		map_[row][column] = tileType;  // ブロック(道:1,軌跡:2)を指定位置に配置
 
-		//blocks_[row][column].imagePos = { tileType,0 }; //軌跡ブロックを表示//最終的にはこれのみ
-		blocks_[row][column].color = 0xffffff66; //仮の軌跡ブロック
+		blocks_[row][column].imagePos = { tileType, 0 }; //軌跡ブロックを表示
 	}
 }
