eac4b119dfc40fee1f92c44db437920a0234e5eb
k024g0006@g.neec.ac.jp <k024g0006@g.neec.ac.jp>
2025-01-23 10:18:47 +0900

    スピードの変更

--- Player.cpp
+++ Player.cpp
@@ -35,7 +35,7 @@ Player::Player(Map& m, int startX, int startY) : m_(m) {
 	isAlive_ = true;
 
 	// speed
-	speed_ = 5;
+	speed_ = 3;
 
 	// 軌跡用
 	moveLength_ = { 0, 0 };
--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int const kWindowHeight = 1080;//windowの縦幅
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 	//フルスクリーン表示 ※これで少し重くなるっぽい
-	//Novice::SetWindowMode(kFullscreen);
+	Novice::SetWindowMode(kFullscreen);
 
 	//確率(rand)
 	unsigned int currentTime = unsigned(time(nullptr));//乱数
@@ -32,7 +32,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Player* player = new Player(*map, 1, 1);
 	Mie* mie = new Mie(11,10);
 	Mie* mie2 = new Mie(13, 10);
-	//Moo* moo = new Moo(15, 10);
+	Moo* moo = new Moo(15, 10);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -93,8 +93,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		CleanTlale(mie, map);
 		CleanTlale(mie2, map);
 
-		//moo->Move(map->map_, player);
-		//moo->Update();
+		moo->Move(map->map_, player);
+		moo->Update();
 
 		///
 		/// ↑更新処理ここまで
@@ -133,7 +133,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			}
 		}
 
-		//moo->Draw();
+		moo->Draw();
 
 		///
 		/// ↑描画処理ここまで
